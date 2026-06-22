c31dd6b1b8b637fc9e9c9485c971580c87440ce9
Paulo <paulocavalcante879@gmail.com>
2026-06-22 18:06:38 -0300

    Conectando a Z-API para o enviou de mensagen com base nos contatos do banco Supabase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.database import get_contacts
+from src.zapi import send_message
 
 def main():
     contacts = get_contacts(limit=3)
@@ -6,8 +7,17 @@ def main():
     if not contacts:
         print("Nenhum contato encontrado.")
         return
-    else:
-        print(contacts)
+    for contact in contacts:
+        name = contact["name"]
+        phone = contact["phone"]
+
+        message = f"Olá, {name} tudo bem com você?"
+
+        try:
+            send_message(phone, message)
+            print(f"Mensagem enviada para {name} ({phone})")
+        except Exception as e:
+            print(f"Erro ao enviar para {name}: {e}")
 
 if __name__ == "__main__":
     main()    
---
+++ src/zapi.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+INSTANCE_ID = os.getenv("ZAPI_INSTANCE_ID")
+TOKEN = os.getenv("ZAPI_TOKEN")
+CLIENT_TOKEN = os.getenv("ZAPI_CLIENT_TOKEN")
+
+
+def send_message(phone, message):
+    url = (
+        f"https://api.z-api.io/instances/{INSTANCE_ID}"
+        f"/token/{TOKEN}/send-text"
+    )
+
+    payload = {
+        "phone": phone,
+        "message": message
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(
+        url,
+        json=payload,
+        headers=headers,
+        timeout=30
+    )
+
+    response.raise_for_status()
+
+    return response.json()
